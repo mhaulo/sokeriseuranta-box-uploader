eb8628fe77825039e10c2fda20e5a55e817b25f9
Mika Haulo <mika@haulo.fi>
2017-01-28 21:44:52 +0200

    Do a big and ugyl bundle-update to make things actually work
    Yes, this sucks, but I'm feeling lazy.

--- sokeriseuranta-box-uploader.py
+++ sokeriseuranta-box-uploader.py
@@ -34,49 +34,6 @@ import re
 import requests
 import json
 
-version = "0.1"
-
-DEFAULT_LOG_FILE = "/tmp/python-usb-wixel.log"
-
-# Sokeriseuranta API info. These are read from a config file
-api_endpoint = ""
-api_token = ""
-user_email = ""
-
-
-# If you wired your wixel directly to the serial pins of the raspberry Pi set this to True
-# for usb connected wixels leave it set as False
-
-use_raspberry_pi_internal_serial_port = False
-
-# Or you can store the configuration in a file which overrides whatever is set in this script
-
-config = SafeConfigParser({'api_endpoint': api_endpoint,
-						   'api_token': api_token,
-						   'user_email': user_email,
-						   'use_raspberry_pi_internal_serial_port': False,
-						   'DEFAULT_LOG_FILE': DEFAULT_LOG_FILE})
-
-# script should be python-usb-wixel.py and then config file will be python-usb-wixel.cfg
-#config_path = re.sub(r".py$", ".cfg", os.path.realpath(__file__))
-config_path="/home/pi/Documents/python-usb-wixel-xdrip/python-usb-wixel.cfg"
-
-if (os.path.isfile(config_path)):
-	config.read(config_path)
-	logger.info("Loading configuration from: " + config_path)
-	api_endpoint = config.get('main', 'api_endpoint').strip()
-	user_email = config.get('main', 'user_email').strip()
-	api_token = config.get('main', 'api_token').strip()
-	
-	try:
-		use_raspberry_pi_internal_serial_port = config.getboolean('main', 'use_raspberry_pi_internal_serial_port')
-	except:
-		use_raspberry_pi_internal_serial_port = False
-		
-	DEFAULT_LOG_FILE = config.get('main', 'DEFAULT_LOG_FILE').strip()
-else:
-	print "Config file " + config_path + " not found, using default values"
-	logger.info("Config file " + config_path + " not found, using default values")
 
 
 # output template
@@ -84,10 +41,9 @@ mydata = {"TransmitterId": "0", "_id": 1, "CaptureDateTime": 0, "RelativeTime": 
 		  "RawValue": 0, "TransmissionId": 0, "BatteryLife": 0, "UploadAttempts": 0, "Uploaded": 0,
 		  "UploaderBatteryLife": 0, "FilteredValue": 0}
 
-
 # threads
 
-def serialThread(dummy):
+def main_loop(api_endpoint, user_email, api_token, use_raspberry_pi_internal_serial_port, logger):
 	logger.info("entering serial loop - waiting for data from wixel")
 	global mydata
 	
@@ -127,21 +83,25 @@ def serialThread(dummy):
 					logger.info("Detected loss of serial sync - restarting")
 					break
 
+				raw_value = datax[0]
+				battery_value = datax[1]
 				# update dictionary - no sanity checking here
-				mydata['CaptureDateTime'] = str(int(time.time())) + "000"
-				mydata['RelativeTime'] = "0"
-				mydata['TransmitterId'] = datax[0]
-				mydata['RawValue'] = datax[1]
-				mydata['FilteredValue'] = datax[2]
-				mydata['BatteryLife'] = datax[3]
-				mydata['ReceivedSignalStrength'] = datax[4]
-				mydata['TransmissionId'] = datax[5]
-
-				upload_data()
+				#mydata['CaptureDateTime'] = str(int(time.time())) + "000"
+				#mydata['RelativeTime'] = "0"
+				#mydata['TransmitterId'] = datax[0]
+				#mydata['RawValue'] = raw_value
+				#mydata['FilteredValue'] = datax[2]
+				#mydata['BatteryLife'] = datax[3]
+				#mydata['ReceivedSignalStrength'] = datax[4]
+				#mydata['TransmissionId'] = datax[5]
+				bg_value = str(raw_to_bg(raw_value, raw_value))
+				
+				if int(raw_value) > 0:
+					upload_data(api_endpoint, user_email, api_token, raw_value, raw_value, bg_value, 0)
 
 			except Exception, e:
 				print "Exception: ", e
-				logger.exception("Exception: ", e)
+				#logger.exception("Exception: ", e)
 
 		except serial.serialutil.SerialException, e:
 			print "Serial exception ", e
@@ -157,10 +117,7 @@ def serialThread(dummy):
 		time.sleep(6)
 		
 
-def upload_data():
-	global api_endpoint
-	global mydata
-
+def upload_data(api_endpoint, user_email, api_token, raw_value, filtered_value, bg_value, battery_life):
 	if api_endpoint != "":
 		tries = 0
 		max_tries = 2
@@ -170,18 +127,13 @@ def upload_data():
 			tries = tries + 1
 			buffer = StringIO()
 			entry_date = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-			bg_value = str(raw_to_bg(mydata['RawValue'], mydata['FilteredValue']))
-			sensor_raw = mydata['RawValue']
-			
+						
 			print "Data at " + entry_date + ": "
-			print "RawValue: " + mydata['RawValue']
-			#print "FilteredValue: " + mydata['FilteredValue']
-			#print "Transmitter ID: " + mydata['TransmitterId']
-			#print "Battery life: " + mydata['BatteryLife']
+			print "RawValue: " + raw_value
 			print "BG value: " + bg_value
 			print "\n"
 			
-			data = '{"log_entries": [{"log_entry": {"date": "' + entry_date + '", "value": "' + bg_value + '", "sensor_raw": "' + sensor_raw + '", "entry_type": "sensor_bg" }}]}'
+			data = '{"log_entries": [{"log_entry": {"date": "' + entry_date + '", "value": "' + bg_value + '", "sensor_raw": "' + raw_value + '", "entry_type": "sensor_bg" }}]}'
 						
 			print "Sending data:"
 			print data
@@ -192,6 +144,7 @@ def upload_data():
 			print "RESPONSE:"
 			print response.json()
 			
+			# TODO check response
 			success = True
 			
 def raw_to_bg(raw_value, filtered_value):
@@ -199,48 +152,85 @@ def raw_to_bg(raw_value, filtered_value):
 	raw = (int(raw_value) + int(filtered_value)) / 2
 	return float(raw) / float(1250*18)
 
-# threads end
+def init():
+	DEFAULT_LOG_FILE = "/tmp/sokeriseuranta-box-uploader.log"
+	logger = logging.getLogger('sokeriseuranta-box-uploader')
+	hdlr = logging.FileHandler(DEFAULT_LOG_FILE)
+	formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+	hdlr.setFormatter(formatter)
+	logger.addHandler(hdlr)
+	logger.addHandler(logging.StreamHandler())
+	logger.setLevel(logging.INFO)
+	
+	api_endpoint=""
+	user_email=""
+	api_token=""
+	use_raspberry_pi_internal_serial_port=True
 
-# MAIN
+	config = SafeConfigParser({
+		'api_endpoint': api_endpoint,
+	    'api_token': api_token,
+	    'user_email': user_email,
+	    'use_raspberry_pi_internal_serial_port': False,
+	    'DEFAULT_LOG_FILE': DEFAULT_LOG_FILE})
 
-# some init
+	config_path="./sokeriseuranta-box-uploader.cfg"
 
-if os.getuid() == 0:
-	logger.info("Dropping root")
-	os.setgid(1000)  # make sure this user is in the dialout group or setgid to dialout
-	try:
-		os.setgid(grp.getgrnam("dialout").gr_gid)
-	except:
-		logger.exception("Couldn't find the dialout group to use")
-
-	os.setuid(1000)
+	if (os.path.isfile(config_path)):
+		config.read(config_path)
+		logger.info("Loading configuration from: " + config_path)
+		api_endpoint = config.get('main', 'api_endpoint').strip()
+		user_email = config.get('main', 'user_email').strip()
+		api_token = config.get('main', 'api_token').strip()
+		
+		try:
+			use_raspberry_pi_internal_serial_port = config.getboolean('main', 'use_raspberry_pi_internal_serial_port')
+		except:
+			use_raspberry_pi_internal_serial_port = False
+			
+		DEFAULT_LOG_FILE = config.get('main', 'DEFAULT_LOG_FILE').strip()
+	else:
+		print "Config file " + config_path + " not found, using default values"
+		logger.info("Config file " + config_path + " not found, using default values")
+	
+	logger.info("Starting up")
 	
 	if os.getuid() == 0:
-		logger.error("Cannot drop root - exit!")
-		sys.exit()
-	else:
-		logger.info("Dropped to user: ", os.getuid())
+		logger.info("Dropping root")
+		os.setgid(1000)  # make sure this user is in the dialout group or setgid to dialout
+		try:
+			os.setgid(grp.getgrnam("dialout").gr_gid)
+		except:
+			logger.exception("Couldn't find the dialout group to use")
+
+		os.setuid(1000)
+		
+		if os.getuid() == 0:
+			logger.error("Cannot drop root - exit!")
+			sys.exit()
+		else:
+			logger.info("Dropped to user: ", os.getuid())
 			
-logger = logging.getLogger('python-usb-wixel')
-hdlr = logging.FileHandler(DEFAULT_LOG_FILE)
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
+	return api_endpoint, user_email, api_token, use_raspberry_pi_internal_serial_port, logger
+				
+	
+if __name__ == '__main__':
+	version = "0.1"
+	print "Sokeriseuranta Box - version " + version
+	
+	# If you wired your wixel directly to the serial pins of the raspberry Pi set this to True
+	# for usb connected wixels leave it set as False
+	use_raspberry_pi_internal_serial_port = False
 
-logger.info("Starting up")
-print "Sokeriseuranta Box - version " + version
-
-# start a thread to listen for serial data
-threading.Thread(target=serialThread, args=("",)).start()
-
-# main busy loop
-try:
-	while(1):
-		sleep(2)	
-
-except KeyboardInterrupt:
-	print "Shutting down"
-	logger.info("Shutting down")
-	os.kill(os.getpid(), signal.SIGKILL)
+	# Sokeriseuranta API info. These are read from a config file
+	api_endpoint = ""
+	api_token = ""
+	user_email = ""
+	
+	try:
+		api_endpoint, user_email, api_token, use_raspberry_pi_internal_serial_port, logger = init()
+		main_loop(api_endpoint, user_email, api_token, use_raspberry_pi_internal_serial_port, logger)
+	except KeyboardInterrupt:
+		print "Shutting down"
+		logger.info("Shutting down")
+		sys.exit(0)
